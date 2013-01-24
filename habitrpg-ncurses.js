48345de63c2534750b8f1856fdf364975e300e68
Rob S <robscanlon@gmail.com>
2013-01-24 00:44:13 -0500

    Major update.  Mostly functional prototype.

--- habitrpg-ncurses.js
+++ habitrpg-ncurses.js
@@ -13,26 +13,24 @@ var data = {
         {name:'1 Hour Productive Work',up:1,down:0},
         {name:'Eat Healthy Meal',up:1,down:1},
         {name:'Junk food / soda',up:0,down:1},
-        {name:'Use Personal Notebook',up:1,down:1},
+        {name:'Use Notebook',up:1,down:1},
         {name:'Visit Reddit / HN',up:0,down:1}
         ],
     daily: [
         {name:'Go to Gym',done:0},
         {name:'Clean Desk, no VM, etc',done:1},
-        {name:'Floss',done:0},
         {name:'In Work Early',done:0},
         {name:'Weekly Summary',done:0},
-        {name:'Record Summary',done:0}
         ],
     todos: [
-        {name:'Ring Insurance'},
-        {name:'book ZQN -> AKL flight'},
-        {name:'confirm all flights'},
-        {name:'email caddy and mullin about stuff to do during the day'},
-        {name:'Review IB'},
-        {name:'Review IB Addendum'},
-        {name:'Review ITM'}],
-    rewards: [
+        {name:'Ring Insurance', done:0},
+        {name:'book ZQN -> AKL flight', done:0},
+        {name:'confirm all flights', done:0},
+        {name:'Review IB', done:0},
+        {name:'Review ITM', done:0},
+        {name:'Call Marriot', done:0},
+        {name:'Refactor habitrpg-ncurses', done:0}],
+    rewards: [   /* NOT IMPLEMENTED YET */
         { name: 'Leather Armor', 'price': 30, description: 'Helps with stuff'},
         { name: 'Sword 2', 'price': 25, description: 'Helps with stuff'},
         { name: 'potion', 'price': 25, description: 'Helps with stuff'}
@@ -40,30 +38,25 @@ var data = {
 
 };
 
-var habitsLocation = {"top": 0, "bottom": 0};
-var dailyLocation = {"top":0, "bottom":0};
 
-//updateHeader(win,'hi here', {pos:'right'});
+var items = [];
 
 nc.colorPair(1,nc.colors.BLACK,nc.colors.WHITE);
 nc.colorPair(2,nc.colors.WHITE,nc.colors.BLACK);
-/*
-win.bkgd = nc.colorPair(1);
-win.attron(nc.colorPair(1));
-win2.attron(nc.colorPair(2));
-win.attron(nc.colorPair(2));
-*/
-//win.scrollok(true);
-//win.setscrreg(1, win.height-3); // Leave one line at the top for the header
-//win.idlok(true);
+nc.colorPair(3,nc.colors.WHITE,nc.colors.CYAN);
+var statusWindow = new nc.Window(7,nc.cols-4);
+var currentIndex = 0;
+var unsaved = false;
 
-/* draw the header */
-    
-    drawHeader(win,"connected");
+var drawFn = function(){
+    items = [];
 
-    /* draw the status box */
-    var statusWindow = new nc.Window(7,nc.cols-4);
-    statusWindow.move(3,2);
+    /* draw the header */
+        
+    drawFooter(win,(unsaved?'unsaved':''));
+
+        /* draw the status box */
+    statusWindow.move(1,2);
     statusWindow.box();
     statusWindow.cursor(0,2);
     statusWindow.addstr(config.username);
@@ -72,82 +65,228 @@ win.attron(nc.colorPair(2));
     drawBar(statusWindow,1,2,data.health,data.healthMax,'Health',2);
     drawBar(statusWindow,1,2,data.exp,data.expMax,'Exp',4);
 
-    /* draw the habit area */
-    var habitWindow = new nc.Window(4+data.habits.length,nc.cols-4);
-    habitWindow.move(11,2);
-    habitWindow.cursor(0,0);
-    habitWindow.hline(habitWindow.width, nc.ACS.HLINE);
-    habitWindow.cursor(0,1);
-    habitWindow.addstr(' Habits ');
-    habitWindow.cursor(1,0);
+    win.cursor(9,0);
+    var habitsDone = 0;
+
+    win.cursor(win.cury+1,0);
     for(var i = 0; i<data.habits.length;i++){
-        habitWindow.cursor(habitWindow.cury+1,0);
-        data.habits[i].cury = habitWindow.cury;
-        habitWindow.addstr('[ ] ' + data.habits[i].name.substr(0,habitWindow.width-5));
+        win.cursor(win.cury+1,2);
+        data.habits[i].cury = win.cury;
+        data.habits[i].type = 'habits';
+        items.push(data.habits[i]);
+        win.addstr('[' + (data.habits[i].up - data.habits[i].down == 0?' ':'' + Math.abs(data.habits[i].up - data.habits[i].down)) + '] ' + data.habits[i].name.substr(0,win.width-5));
+
+        if (data.habits[i].up - data.habits[i].down < 0){
+            win.cursor(win.cury,3);
+            win.addstr('-');
+            //win.chgat(win.cury, 3, 1, nc.attrs.NORMAL, nc.colorPair(3));
+
+        } 
+        habitsDone += data.habits[i].up;
+        habitsDone -= data.habits[i].down;
+        
+        
+    }
+    win.cursor(win.cury-data.habits.length-1,0);
+    drawHeader(win,"Habits [" + habitsDone + " today]");
+    win.cursor(win.cury+data.habits.length+4,0);
+
+    var dailyDone = 0;
+
+    for(var i = 0; i<data.daily.length;i++){
+        win.cursor(win.cury+1,2);
+        data.daily[i].cury = win.cury;
+        data.daily[i].type = 'daily';
+        items.push(data.daily[i]);
+        win.addstr('[' + (data.daily[i].done?'X':' ') + '] ' + data.daily[i].name.substr(0,win.width-5));
+        dailyDone += data.daily[i].done;
+        
 
     }
-    /* draw the Daily section*/
-    var dailyWindow = new nc.Window(4,nc.cols-4);
-    dailyWindow.move(habitWindow.begy+habitWindow.height-1,2);
-    dailyWindow.cursor(0,0);
-    dailyWindow.hline(dailyWindow.width, nc.ACS.HLINE);
-    dailyWindow.cursor(0,1);
-    dailyWindow.addstr(' Daily ');
-    dailyWindow.cursor(2,0);
-    for(var i = data.daily.length-1; i>=0;i--){
-        dailyWindow.insertln();
-        dailyWindow.resize(dailyWindow.height+1, dailyWindow.width);
-        dailyWindow.cursor(2,0);
-        dailyWindow.addstr('[ ] ' + data.daily[i].name.substr(0,dailyWindow.width-5));
+    win.cursor(win.cury-data.daily.length-1,0);
+    drawHeader(win,"Daily [" + dailyDone + " complete]");
+    win.cursor(win.cury+data.daily.length+4,0);
 
+    var todosDone = 0;
+
+    for(var i = 0; i<data.todos.length;i++){
+        win.cursor(win.cury+1,2);
+        data.todos[i].cury = win.cury;
+        data.todos[i].type = 'todos';
+        items.push(data.todos[i]);
+        win.addstr('[' + (data.todos[i].done?'X':' ') + '] ' + data.todos[i].name.substr(0,win.width-5));
+        todosDone += data.todos[i].done;
     }
 
-    var todoWindow = new nc.Window(4,nc.cols-4);
-    todoWindow.move(dailyWindow.begy+dailyWindow.height-1,2);
-    todoWindow.cursor(0,0);
-    todoWindow.hline(todoWindow.width, nc.ACS.HLINE);
-    todoWindow.cursor(0,1);
-    todoWindow.addstr(' Todos ');
-    todoWindow.cursor(2,0);
-    for(var i = data.todos.length-1; i>=0;i--){
-        todoWindow.insertln();
-        todoWindow.resize(todoWindow.height+1, todoWindow.width);
-        todoWindow.cursor(2,0);
-        todoWindow.addstr('[ ] ' + data.todos[i].name.substr(0,todoWindow.width-5));
-    }
-win.cursor(0,0);
+
+    win.cursor(win.cury-data.todos.length-1,0);
+    drawHeader(win,"Todos [" + todosDone + " completed today]");
+    win.cursor(win.cury+data.todos.length+4,0);
+
+    win.chgat(items[currentIndex].cury, 2, win.width-5, nc.attrs.STANDOUT, nc.colorPair(5));
     win.refresh();
-    //win.cursor(0,0);
+}
 
-    currentIndex = 0;
-        habitWindow.chgat(data.habits[0].cury, 0, habitWindow.width, nc.attrs.STANDOUT, nc.colorPair(5));
-        habitWindow.cursor(data.habits[0].cury,2);
-        habitWindow.refresh();
-todoWindow.on('inputChar', function (c, i) {
-    if(i === 106){
+drawFn();
+
+function drawHeader(mywin, title){
+    mywin.hline(win.width-3, nc.ACS.HLINE);
+    mywin.cursor(mywin.cury,0);
+    mywin.addstr('  ');
+    mywin.cursor(mywin.cury,3);
+    mywin.addstr(' ' + title + ' ');
+    mywin.cursor(mywin.cury,0);
+}
 
 
-        habitWindow.chgat(data.habits[currentIndex].cury, 0, habitWindow.width, nc.attrs.NORMAL, nc.colorPair(0));
-        currentIndex++;
-        
-        habitWindow.chgat(data.habits[currentIndex].cury, 0, habitWindow.width, nc.attrs.STANDOUT, nc.colorPair(5));
-        habitWindow.cursor(data.habits[currentIndex].cury,2);
-        habitWindow.refresh();
+var inputWindow = new nc.Window(1,nc.cols);
+var mode = 'normal';
+nc.showCursor = false;
+inputWindow.move(win.height-1,0);
+inputWindow.refresh();
+
+inputWindow.on('inputChar', function (c, i) {
+
+    if(mode == 'normal'){
+        inputWindow.clear();
+
+        if(i === 106){
+
+
+            win.chgat(items[currentIndex].cury, 2, win.width-5, nc.attrs.NORMAL, nc.colorPair(0));
+            currentIndex++;
+            if(currentIndex > items.length -1){
+                currentIndex = 0;
+            }
+            
+            win.chgat(items[currentIndex].cury, 2, win.width-5, nc.attrs.STANDOUT, nc.colorPair(5));
+            win.cursor(items[currentIndex].cury,2);
+            win.refresh();
+
+        } else if(i === 107){
+
+            win.chgat(items[currentIndex].cury, 2, win.width-5, nc.attrs.NORMAL, nc.colorPair(0));
+            currentIndex--;
+            if(currentIndex < 0){
+                currentIndex = items.length-1;
+            }
+            
+            win.chgat(items[currentIndex].cury, 2, win.width-5, nc.attrs.STANDOUT, nc.colorPair(5));
+            win.cursor(items[currentIndex].cury,2);
+            win.refresh();
+
+        } else if(i === 105){
+            unsaved = true;
+            if(items[currentIndex].type == 'daily' || items[currentIndex].type == 'todos'){
+                items[currentIndex].done = 1;
+                process.nextTick(drawFn);
+
+            } else if(items[currentIndex].type == 'habits'){
+                items[currentIndex].up += 1;
+                process.nextTick(drawFn);
+            }
+
+        } else if(i === 120 || i == 32){
+            if(items[currentIndex].type == 'daily' || items[currentIndex].type == 'todos'){
+                unsaved = true;
+                items[currentIndex].done = (items[currentIndex].done * -1) + 1;
+                process.nextTick(drawFn);
+            }
+
+        } else if(i === 100){
+            unsaved = true;
+            if(items[currentIndex].type == 'daily' || items[currentIndex].type == 'todos'){
+                items[currentIndex].done = 0;
+                process.nextTick(drawFn);
+            } else if(items[currentIndex].type == 'habits'){
+                items[currentIndex].down += 1;
+                process.nextTick(drawFn);
+            }
+        }  else if(i === 58){
+            mode = 'command';
+            inputWindow.inbuffer = '';
+            nc.showCursor = true;
+
+        }
+    }
+
+    if(mode == 'command'){
+        if(i === 9){
+            mode = 'normal';
+            inputWindow.inbuffer = '';
+            nc.showCursor = false;
+            inputWindow.refresh();
+        }else if(i === 330){
+
+            var prev_x = inputWindow.curx;
+            inputWindow.delch(inputWindow.height-1, inputWindow.curx);
+            inputWindow.inbuffer = inputWindow.inbuffer.substring(0, inputWindow.curx-1) + inputWindow.inbuffer.substring(inputWindow.curx);
+            inputWindow.cursor(inputWindow.height-1, prev_x);
+            if(inputWindow.inbuffer.length == 0){
+                mode = 'normal';
+                nc.showCursor = false;
+            }
+            inputWindow.refresh();
+
+        } else if (i === 127 && inputWindow.curx > 0) {
+            var prev_x = inputWindow.curx-1;
+            inputWindow.delch(inputWindow.height-1, prev_x);
+            inputWindow.inbuffer = inputWindow.inbuffer.substring(0, prev_x) + inputWindow.inbuffer.substring(prev_x+1);
+            inputWindow.cursor(inputWindow.height-1, prev_x);
+            if(inputWindow.inbuffer.length == 0){
+                mode = 'normal';
+                nc.showCursor = false;
+            }
+            inputWindow.refresh();
+        } else if (i === nc.keys.NEWLINE) {
+            if (inputWindow.inbuffer.length) {
+
+                if (inputWindow.inbuffer[0] === ':') {
+                    var cmd = inputWindow.inbuffer.substring(1).split(' ', 1).join('').trim(),
+                        args = inputWindow.inbuffer.substring(inputWindow.inbuffer.indexOf(cmd)+cmd.length+1).trim();
+                    switch (cmd.toLowerCase()) {
+                        case 'a':
+                            if (args.length) {
+                                data.todos.push({'name':args, done:0});
+                                inputWindow.clear();
+                                inputWindow.inbuffer = '';
+                                process.nextTick(drawFn);
+                                nc.showCursor = false;
+                                mode = 'normal';
+                                unsaved = true;
+                            
+                            }
+                            break;
+
+                        case 'q':
+                            nc.cleanup();
+                            process.exit(0);
+                            break;
+                        default:
+                            inputWindow.clear();
+                            inputWindow.addstr('Unknown command: ' + cmd);
+                            mode = 'normal';
+                            nc.showCursor = false;
+                            
+                    }
+                }
+            }
+
+        } else if (i >= 32 && i <= 126 && inputWindow.curx < inputWindow.width-4) {
+           inputWindow.echochar(i);
+           inputWindow.inbuffer += c;
+        }
 
     }
-    if(i === 107){
-
-
-        habitWindow.chgat(data.habits[currentIndex].cury, 0, habitWindow.width, nc.attrs.NORMAL, nc.colorPair(0));
-        currentIndex--;
+    inputWindow.refresh();
         
-        habitWindow.chgat(data.habits[currentIndex].cury, 0, habitWindow.width, nc.attrs.STANDOUT, nc.colorPair(5));
-        habitWindow.cursor(data.habits[currentIndex].cury,2);
-        habitWindow.refresh();
+    //inputWindow.addstr('' +i);
 
-    }
+
 
 });
+
+
 win.on('inputChar', function (c, i) {
     win.addstr('hi there');
     win.refresh();
@@ -170,13 +309,12 @@ win.on('inputChar', function (c, i) {
 });
 */
 
-function drawHeader(mywin, state){
-    mywin.cursor(0,0);
+function drawFooter(mywin, state){
+    mywin.cursor(mywin.height-2,0);
     mywin.clrtoeol();
     win.addstr("HabitRPG");
-    mywin.addstr(0, mywin.width-(Math.min(state.length, mywin.width)), state, mywin.width);
-    mywin.cursor(1,0);
-    mywin.hline(nc.cols, nc.ACS.HLINE);
+    mywin.addstr(mywin.height-2, mywin.width-(Math.min(state.length, mywin.width)), state, mywin.width);
+    mywin.chgat(mywin.height-2, 0, mywin.width, nc.attrs.STANDOUT, nc.colorPair(5));
 }
 
 
@@ -199,28 +337,4 @@ function drawBar(mywin, onColorPair, offColorPair, val, valMax, label, rowStart)
     mywin.attroff(nc.colorPair(2));
     mywin.refresh();
 }
-function updateHeader(win, header, style) {
-   var curx = win.curx, cury = win.cury;
-   style = style || {};
-   header = '' + header;
-   win.cursor(0, 0);
-   win.clrtoeol();
-   if (style.attrs)
-     win.attron(style.attrs);
-   if (header.length > 0) {
-     if (style.pos === 'center')
-       win.centertext(0, header);
-     else if (style.pos === 'right')
-       win.addstr(0, win.width-(Math.min(header.length, win.width)), header, win.width);
-     else
-        win.addstr(header, win.width);
-  }
-   if (style.attrs)
-     win.attroff(style.attrs);
-    win.cursor(1,0);
-    win.hline(nc.cols, nc.ACS.HLINE);
-    win.refresh();
-   win.cursor(cury, curx);
-   win.refresh();
-}
-//process.on('SIGWINCH',fnDraw);
+process.on('SIGWINCH',drawFn);
